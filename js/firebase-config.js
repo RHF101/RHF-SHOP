4fdd7725c537d43e706ea2090e1e8b73740206f1
RHF101 <rtiya980@gmail.com>
2026-04-12 22:27:17 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,28 @@
+/**
+ * RHF STORZ - Firebase Configuration
+ * Dedicated for Asia-Southeast1 (Singapore) Region
+ */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVqKwHmRBxWu5hjulwmhradnGbp0yFhbY",
+    authDomain: "rhf-shop.firebaseapp.com",
+    databaseURL: "https://rhf-shop-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "rhf-shop",
+    storageBucket: "rhf-shop.firebasestorage.app",
+    messagingSenderId: "1065902539657",
+    appId: "1:1065902539657:web:ccb3cbdbecdcf81eb81758",
+    measurementId: "G-HLNZZ2E26F"
+};
+
+// Inisialisasi Firebase agar tidak terjadi error double-init
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+// Inisialisasi Service Utama
+const auth = firebase.auth();
+const db = firebase.database();
+
+// Log status untuk debugging di konsol browser
+console.log("RHF-Core: Firebase Engine Connected.");
 
